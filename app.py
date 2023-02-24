7a65188d6042ee454326380381ee5ca0bc17c4c6
RodolfoTorija <112714490+RodolfoTorija@users.noreply.github.com>
2023-02-24 11:57:12 -0600

    login logueado

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
-from flask import Flask , render_template, request, redirect, url_for
-from config import config
+from flask import Flask, render_template, request, redirect, url_for, flash
+import mysql.connector
 from flask_mysqldb import MySQL
+from jinja2 import Template
 
-app=Flask(__name__)
-
-db = MySQL(app)
-
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-@app.route("/login" , methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        request.form['username']
-        request.form['password']
-        return render_template("login.html")
-    
-    else:
-
-        return render_template("login.html")
+app = Flask(__name__)
 
 
 
 
 
-
-
-@app.route('/home.html')
+@app.route("/home.html")
 def home():
     return render_template("home.html")
+
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+@app.route('/index.html')
+def index_inicio():
+    return render_template("index.html")
 
 @app.route('/especialidades.html')
 def especialidades():
@@ -49,10 +39,60 @@ def contactanos():
     return render_template("contactanos.html")
 
 
+# Configuración de la base de datos
+...
 
-if __name__ =='__main__':
-    app.config.from_object(config['development'])
+
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        port = "3306",
+        user= "root",
+        password = "Zeromainj0.",
+        database = "clinica_azul"
+    )
+
+    print("Conexión exitosa a la base de datos. \n")
+
+except mysql.connector.Error as error:
+    print("Error al conectarse a la base de datos: {}".format(error))
+    mydb = None
+
+#Conexion de base de datos
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if mydb is None:
+        return render_template('/login.html', mensaje='Error al conectarse a la base de datos')
+
+    nombre = request.form['username']
+    contrasena = request.form['password']
+
+    try:
+        mycursor = mydb.cursor()
+
+        mycursor.execute("SELECT * FROM usuarios WHERE nombre = %s AND contrasena = %s", (nombre, contrasena))
+
+        usuario = mycursor.fetchone()
+
+        if usuario:
+            print("Inicio correctametne")
+            
+            #alerta con flash
+           
+            # Inicio de sesión exitoso
+            return render_template('/home.html', correcto='Has iniciado sesión correctamente')
+        else:
+            print("No se inicio correctamente")
+            # Nombre de usuario o contraseña incorrectos
+            return render_template('/login.html', error='Nombre de usuario o contraseña incorrectos')
+
+    except mysql.connector.Error as error:
+        print("Error al ejecutar la consulta a la base de datos: {}".format(error))
+        return render_template('/login.html', error='Error al ejecutar la consulta a la base de datos')
+
+
+
+
+if __name__ == '__main__':
     app.run()
-
-
 
